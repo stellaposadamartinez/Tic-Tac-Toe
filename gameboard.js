0731ec99d3a60eeef0ff07903469e0c9e5f786cf
stellaposadamartinez <stellaposadamartinez@gmail.com>
2021-10-15 09:43:43 +0200

    commit

--- gameboard.js
+++ gameboard.js
@@ -1,39 +1,12 @@
 class gameboard {
-  constructor() { //boardWidth, boardHeight, numColumns, numRows
-    //this.fields = [];
-    //for(let i = 0; i < numRows; i < numColumns);
-    this.letters = ["A", "B", "C"];
+  constructor() {
   }
 
   draw() {   
-    //loop zur nummerierung der felder mit einer for-schleife
-
-    //Felder
     gameboardfields(0, 0);
-
-    //nummerierung der felder
-    //this.fieldnumber("A1", 80, 95);
-    //this.fieldnumber("B1", 180, 95);
-    //this.fieldnumber("C1", 280, 95);
-
-    //this.fieldnumber("A2", 80, 195);
-    //this.fieldnumber("B2", 180, 195);
-    //this.fieldnumber("C2", 280, 195);
-
-    //this.fieldnumber("A3", 80, 295);
-    //this.fieldnumber("B3", 180, 295);
-    //this.fieldnumber("C3", 280, 295);
   }
   
   eingabe() {
-    if(keyIsPressed) {
-      if(key == 'a' && '') {
-        textSize(50);
-        text('X', 45, 55);
-      } else if (key == 'a' + '2') {
-        
-      }
-    }
   }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,29 +1,52 @@
 let felder;
-let nummerierung = [];
-let nummerierungRow = 3;
-let nummerierungColumn = 3;
-let columnNums = "ABCDEFG";
+
+let nummerierungRow = 2;
+let nummerierungColumn = 2;
+let columnNums = "ABCDEF";
+
+let input
+let button
+let feldName
+
+let structure = [];
 
 function setup() {
   let canvasWidth = 300;
-  let canvasHeight = 300;
+  let canvasHeight = 350;
+
+  for (let i = 0; i < nummerierungColumn; i++) {
+    let row = [];
+    for (let j = 0; j < nummerierungRow; j++) {
+      row.push("");
+    }
+    structure.push(row);
+  }
 
   createCanvas(canvasWidth, canvasHeight);
+  background('lightgrey');
+
+  input = createInput();
+  input.position(60, 315);
+
+  button = createButton('submit');
+  button.position(190, 315);
+  button.mousePressed(eingabe);
 
   felder = new gameboard();
   felder.draw();
   
+  fill('black');
+  textSize(15);
   for (var i = 0; i <= nummerierungColumn; i++) {
     for (var j = 0; j <= nummerierungRow; j++) {
-      print('nummerierung:', columnNums[i], j + 1, 80 + i * 100, 95 + i * 100);
-      fill('black');
-      text(columnNums[i] + j + 1, 80 + i * 100, 95 + i * 100);
+      text(columnNums[i] + (j + 1), i * 100 + 70, 95 + j * 100);
     }
   }
 }
 
-function draw() {
-
-  //felder.draw();
-  felder.eingabe();
+function eingabe(){
+  let i = input.value();
+  print(input.value());
+  print('row: ', int(i[1]) - 1);
+  print('column: ', int(i[0]) - 1);
 }
